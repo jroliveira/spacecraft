4b733500264de598f55eaeed6c6467f61ccc1f89
Junior Oliveira <junolive@gmail.com>
2014-04-25 00:36:56 -0300

    Criando poc de chat com socket.io

--- web.js
+++ web.js
@@ -1,16 +1,29 @@
-﻿var requirejs = require('requirejs');
+var requirejs = require('requirejs');
 
 requirejs.config({
     nodeRequire: require
 });
 
 requirejs([
-    'server/app'
-], function (app) {
+    'server/app',
+    'socket.io',
+    'http'
+], function (app, socket, http) {
     
-    var port = process.env.PORT || 4000;
-    app.listen(port, function () {
-        console.log("Listening on " + port);
+    //var port = process.env.PORT || 4000;
+    //app.listen(port, function () {
+    //    console.log("Listening on " + port);
+    //});
+
+    var server = http.createServer(app);
+    var io = socket.listen(server);
+    
+    server.listen(4000);
+    
+    io.sockets.on('connection', function (socket) {
+        socket.emit('news', { hello: 'world' });
+        socket.on('my other event', function (data) {
+            console.log(data);
+        });
     });
-    
 });
